790e5b651206544bee373948d4ca6960ee167938
Roel Monteros <roel.monteros@nagarro.com>
2026-09-24 12:36:08 +0800

    custom theme

---
+++ app/design/frontend/Canon/theme/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright 2015 Adobe
+ * All Rights Reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Canon/theme',
+    __DIR__
+);
